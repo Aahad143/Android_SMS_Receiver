29d2df642ed291b879628f75fa02fce9818a24a2
Yusuf Kaka <yusufk@gmail.com>
2024-06-16 20:52:59 +0200

    updated SDK and got it working on latest SDK

--- app/src/main/java/za/co/mtn/ti/notifier/MainActivity.java
+++ app/src/main/java/za/co/mtn/ti/notifier/MainActivity.java
@@ -2,16 +2,26 @@ package za.co.mtn.ti.notifier;
 
 import com.example.smslistener.R;
 
+import android.Manifest;
+import android.app.NotificationChannel;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
-
+    private NotificationChannel channel;
+    private static final String TAG = "SMSReceiverActivity";
+    private int MY_PERMISSIONS_REQUEST_SMS_RECEIVE = 10;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.RECEIVE_SMS},
+                MY_PERMISSIONS_REQUEST_SMS_RECEIVE);
+        Log.d(TAG,"Started..");
     }
 
     @Override
@@ -19,6 +29,15 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_PERMISSIONS_REQUEST_SMS_RECEIVE) {
+            // YES!!
+            Log.i("TAG", "MY_PERMISSIONS_REQUEST_SMS_RECEIVE --> YES");
+        }
     }
 
 }
--- app/src/main/java/za/co/mtn/ti/notifier/SMSReceiver.java
+++ app/src/main/java/za/co/mtn/ti/notifier/SMSReceiver.java
@@ -1,19 +1,22 @@
 /**
- * 
+ *
  */
 package za.co.mtn.ti.notifier;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
 import android.telephony.SmsMessage;
 import android.util.Log;
 import android.widget.Toast;
 
 import com.example.smslistener.R;
+
+import java.util.Objects;
 
 /**
  * @author yusuf
@@ -21,58 +24,69 @@ import com.example.smslistener.R;
  */
 public class SMSReceiver extends BroadcastReceiver {
 
-	/**
-	 * 
-	 */
-	private static final String DATA_SMS_RECEIVED = "android.intent.action.DATA_SMS_RECEIVED";
-	private static final String TAG = "SMSBroadcastReceiver";
-	
+    /**
+     *
+     */
+    private static final String DATA_SMS_RECEIVED = "android.intent.action.DATA_SMS_RECEIVED";
+    private static final String TAG = "SMSBroadcastReceiver";
+    private NotificationChannel nChannel;
 
-	/* (non-Javadoc)
-	 * @see android.content.BroadcastReceiver#onReceive(android.content.Context, android.content.Intent)
-	 */
-	@Override
-	public void onReceive(Context context, Intent intent) {
-		        
-        
-		Log.i(TAG, "Intent recieved: " + intent.getAction());
-        if (intent.getAction().equals(DATA_SMS_RECEIVED)) {
+    /* (non-Javadoc)
+     * @see android.content.BroadcastReceiver#onReceive(android.content.Context, android.content.Intent)
+     */
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d(TAG, "Intent received: " + intent.getAction());
+        this.nChannel = initChannels(context);
+        if (Objects.equals(intent.getAction(), DATA_SMS_RECEIVED)) {
             Bundle bundle = intent.getExtras();
             if (bundle != null) {
                 Object[] pdus = (Object[])bundle.get("pdus");
+                assert pdus != null;
                 final SmsMessage[] messages = new SmsMessage[pdus.length];
                 for (int i = 0; i < pdus.length; i++) {
-                    messages[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
+                    String format = bundle.getString("format");
+                    messages[i] = SmsMessage.createFromPdu((byte[])pdus[i],format);
+                    Log.d(TAG, "Message received: " + messages[i].getMessageBody());
+                    Toast.makeText(context, messages[i].getMessageBody(), Toast.LENGTH_SHORT).show();
                 }
-                if (messages.length > -1) {
-                    Log.i(TAG, "Message recieved: " + messages[0].getMessageBody());
-                    Toast.makeText(context, messages[0].getMessageBody(), Toast.LENGTH_SHORT).show();
-                    
-                    try {
-                    	 SMSNotification smsnotification = new SMSNotification( messages[0].getMessageBody());
-                         notify(context,smsnotification);
-					} catch (Exception e) {
-						Log.e(TAG,"SMS received cannot be parsed correctly!");
-						e.printStackTrace();
-					}
-                   
+                try {
+                    SMSNotification smsnotification = new SMSNotification( messages[0].getMessageBody());
+                    notify(context,smsnotification, nChannel);
+                } catch (Exception e) {
+                    Log.e(TAG,"SMS received cannot be parsed correctly!");
+                    //e.printStackTrace();
                 }
             }
         }
-	}
-	
-	public void notify(Context context, SMSNotification sms){
-		NotificationCompat.Builder mBuilder =
-		        new NotificationCompat.Builder(context)
-		        .setSmallIcon(R.drawable.mtn_logo)
-		        .setContentTitle(sms.getHeading())
-		        .setContentText(sms.getMessage());
+    }
 
-		NotificationManager mNotificationManager =
-		    (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-		int mId = sms.getId();
-		// mId allows you to update the notification later on.
-		mNotificationManager.notify(mId, mBuilder.build());
-		
-	}
+    public NotificationChannel initChannels(Context context) {
+        NotificationManager notificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationChannel channel = new NotificationChannel("default",
+                "Channel name",
+                NotificationManager.IMPORTANCE_DEFAULT);
+        channel.setDescription("Channel description");
+        notificationManager.createNotificationChannel(channel);
+        return channel;
+    }
+
+    public void notify(Context context, SMSNotification sms, NotificationChannel channel){
+        Notification.Builder mBuilder = new Notification.Builder(context , nChannel.getId())
+                .setSmallIcon(R.drawable.mtn_logo)
+                .setContentTitle(sms.getHeading())
+                .setContentText(sms.getMessage())
+                //.setPriority(Notification.PRIORITY_DEFAULT)
+                // Set the intent that will fire when the user taps the notification
+                //.setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        NotificationManager mNotificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        int mId = sms.getId();
+        // mId allows you to update the notification later on.
+        mNotificationManager.notify(mId, mBuilder.build());
+
+    }
 }
